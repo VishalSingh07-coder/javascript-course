45fc6ab379b61271d90fe5c17274c78b682ef552
VishalSingh07-coder <84367090+VishalSingh07-coder@users.noreply.github.com>
2023-10-01 12:05:29 +0000

    conversion Done

---
+++ basics_01/conversion_operations.js
@@ -0,0 +1,30 @@
+let str="10"
+// console.log(typeof num)
+
+//to convert string to number
+
+let valueInNumber=Number(str)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN(not a number)
+//  true => 1; false => 0
+
+
+//convert number to string
+let num=10
+let valueInStr=String(num)
+// console.log(valueInStr)
+
+//convert string or numkber to boolean
+// let num1=33
+let string=""
+
+let isBoolean=Boolean(string)
+console.log(isBoolean);
+
+//33=>true
+//"Vishal"=>true
+//""=>false
+//1=>true; 0=>false
