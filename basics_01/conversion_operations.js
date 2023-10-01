8a782864ec8bf47cad63a64a67cf4995bafe44c3
VishalSingh07-coder <84367090+VishalSingh07-coder@users.noreply.github.com>
2023-10-01 12:47:33 +0000

    conversion operation Done

--- basics_01/conversion_operations.js
+++ basics_01/conversion_operations.js
@@ -22,9 +22,44 @@ let valueInStr=String(num)
 let string=""
 
 let isBoolean=Boolean(string)
-console.log(isBoolean);
+// console.log(isBoolean);
 
 //33=>true
 //"Vishal"=>true
 //""=>false
 //1=>true; 0=>false
+
+//----------------------Opetation------------------------------------
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**5);
+// console.log(2/3);
+// console.log(2%3);
+
+//concantenate stings
+let str1="Vishal"
+let str2=" Singh"
+
+// console.log(str1+str2);
+
+//arthemetic operations
+// console.log(1+"2"); //12
+// console.log("1"+2); //12
+// console.log(1+1+"2"); //22
+// console.log("1"+2+3); //123
+// console.log(1+"2"+3); //123
+// console.log(+true);
+
+let num1,num2,num3
+num1=num2=num3=2+2 //All variable have 4 value
+// console.log(num1);
+
+let gameCounter=100
+++gameCounter//preincrement
+// gameCounter++ //postincrement
+console.log(gameCounter);
