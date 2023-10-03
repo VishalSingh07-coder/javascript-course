0a6bf527d61636ba79452b4fbe830bbda18be637
VishalSingh07-coder <84367090+VishalSingh07-coder@users.noreply.github.com>
2023-10-03 08:14:30 +0000

    datatypes summary

---
+++ basics_01/datatype_summary.js
@@ -0,0 +1,30 @@
+//primitive datatypes
+
+//7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+let name="Vishal Singh"
+let num=10
+let isLoggedIn=true
+let temp=null
+let username;
+
+let Id=Symbol('123')
+let anotherId=Symbol('123')
+
+// console.log(Id===anotherId);
+let bigNumber=3313232324232n
+
+//reference (non-primitive)
+//Array, Objects, Function
+
+const powerRangers=["SPD","Ninja Furry","Samurai"]
+
+let details={
+    name:"VishalSingh",
+    Id:7
+}
+let myFunction=function()
+{
+    console.log("hello world");
+}
+console.log(typeof myFunction);
