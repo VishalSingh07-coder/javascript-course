e37cec2a3b6f7b52a7df3a7dc8732771ae473cd0
VishalSingh07-coder <84367090+VishalSingh07-coder@users.noreply.github.com>
2023-10-05 23:08:59 +0530

    Delete basics_01/variables.js

--- basics_01/variables.js
+++
@@ -1,7 +0,0 @@
-const accountId=14414
-let accountCity="Nabha"
-var acccountPassword="12345"
-
-console.log(accountId);
-console.table([accountCity,acccountPassword])
-//prefer to use let instead of var because of issue in block scope
