cedae9ab8351caddeab8dca480168caa1cc8fa09
VishalSingh07-coder <84367090+VishalSingh07-coder@users.noreply.github.com>
2023-10-05 17:35:49 +0000

    Name Change

---
+++ basics_01/03_conversion_operations.js
@@ -0,0 +1,65 @@
+let str="10"
+// console.log(typeof num)
+
+//to convert string to number
+
+let valueInNumber=Number(str)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN(not a number)
+//  true => 1; false => 0
+
+
+//convert number to string
+let num=10
+let valueInStr=String(num)
+// console.log(valueInStr)
+
+//convert string or numkber to boolean
+// let num1=33
+let string=""
+
+let isBoolean=Boolean(string)
+// console.log(isBoolean);
+
+//33=>true
+//"Vishal"=>true
+//""=>false
+//1=>true; 0=>false
+
+//----------------------Opetation------------------------------------
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**5);
+// console.log(2/3);
+// console.log(2%3);
+
+//concantenate stings
+let str1="Vishal"
+let str2=" Singh"
+
+// console.log(str1+str2);
+
+//arthemetic operations
+// console.log(1+"2"); //12
+// console.log("1"+2); //12
+// console.log(1+1+"2"); //22
+// console.log("1"+2+3); //123
+// console.log(1+"2"+3); //123
+// console.log(+true);
+
+let num1,num2,num3
+num1=num2=num3=2+2 //All variable have 4 value
+// console.log(num1);
+
+let gameCounter=100
+++gameCounter//preincrement
+// gameCounter++ //postincrement
+console.log(gameCounter);
---
+++ basics_01/05_strings.js
@@ -0,0 +1,26 @@
+const name="Vishal Singh"
+const id=55
+// console.log(name + id + "male"); //old method
+// console.log(`my name is ${name} and my id is ${id}`); //new method
+
+//method to create string
+const str=new String('hello i am here')
+
+// console.log(str[2]);
+// console.log(str.__proto__);
+// console.log(str.toUpperCase());//created in stack
+// console.log(str.indexOf('l'));
+
+// console.log(str.charAt(2));
+// console.log(str.concat( name));
+
+const sentence="the lion runs fast"
+const word="lion"
+// console.log(`the word ${word} ${sentence.includes(word)? 'is' : 'is not'} present in the sentence` ); //include method return true or false
+// console.log( sentence.substring(0,9));
+
+// console.log( sentence.slice(2,10));
+
+const newName=" rolls royce "
+console.log(newName);
+console.log(newName.trim());
