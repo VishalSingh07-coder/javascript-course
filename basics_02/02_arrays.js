6293e9d10918579f1566a7f1aa6b075e19dadfa3
VishalSingh07-coder <84367090+VishalSingh07-coder@users.noreply.github.com>
2023-10-25 13:09:34 +0000

    array new

---
+++ basics_02/02_arrays.js
@@ -0,0 +1,28 @@
+let marvel_heroes=["ironman","thor","hulk","captain america"]
+let dc_heroes=["superman","flash","batman"]
+// marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes);
+
+//to make two arrays a single array we use 2 methods
+//method 1(using concatenate operation)
+
+let heroes=marvel_heroes.concat(dc_heroes)
+// console.log(heroes);
+//method 2
+let new_heroes=[...marvel_heroes,...dc_heroes];
+
+// console.log(new_heroes);
+
+
+let num=[1,2,3,[4,5],6,7,[9,10]]
+// console.log(num.flat(Infinity));
+
+//functions used in scraping
+
+// console.log(Array.isArray("vishal"));
+
+    // console.log(Array.from("vishal"));
+    let id1=1000
+    let id2=1001
+    let id3=1002
+    console.log(Array.of(id1,id2,id3));
